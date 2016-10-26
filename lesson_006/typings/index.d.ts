ff5251a50c609a89d8ca214ca1047a44fce17d0e
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-10-26 08:31:51 -0700

    update

---
+++ lesson_006/typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/ejs/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mssql/index.d.ts" />
+/// <reference path="globals/mysql/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/tedious/index.d.ts" />
